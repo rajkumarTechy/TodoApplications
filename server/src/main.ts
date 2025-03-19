947fc8c044d8e7c38dc47a6fb48f7df2db8e114a
rajkumarTechy <smartmedianetworks01@gmail.com>
2025-03-19 10:17:07 +0530

    header added on main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,11 +4,22 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(cookieParser());
+
   app.enableCors({
-    origin: 'https://todo-applications-tec1.vercel.app',
+    origin: ['https://todo-applications-tec1.vercel.app'],
     credentials: true,
-  })
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'X-Requested-With',
+      'Accept',
+      'Origin'
+    ],
+  });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
